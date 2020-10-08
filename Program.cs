22be7f1d9c870cd34381d7e631d9f3e3e24b79c3
jamae10 <52740452+jamae10@users.noreply.github.com>
2020-10-08 20:53:10 +0800

    displaying main menu table

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace MidTermProj
-{
-    class Program
+namespace MidTermProj//
+{//
+    class MainMenu
     {
+        //Main Menu Table
+        public void displayMainMenu()
+        {
+            Console.WriteLine("================================");
+            Console.WriteLine($"{" ", -12}{"Main Menu", -10}");
+            Console.WriteLine("================================");
+            Console.WriteLine("[1] Arithmetic ");
+            Console.WriteLine("[2] String Problems");
+            Console.WriteLine("[3] Unit Converter");
+            Console.WriteLine("[4] Array Sorting");
+            Console.WriteLine("[5] Math Class");
+            Console.WriteLine("[6] Patterns");
+            Console.WriteLine("[7] Exit");
+            Console.WriteLine("================================");
+
+        }
+
+        //Main Method
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Boolean loop = true;
+            do
+            {
+                MainMenu menu = new MainMenu();
+                menu.displayMainMenu();
+                break;
+            } while (loop);
         }
+
+        
     }
-}
+}//
